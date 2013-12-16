00f5b663da66a74bdd1b7b774db92d891a5f621c
salmantariqmirza <stariqmi@gmail.com>
2013-12-15 23:37:44 -0800

    fix in db seeds

--- mt_rails_app/db/seeds.rb
+++ mt_rails_app/db/seeds.rb
@@ -26,7 +26,7 @@ operators.each do |op|
 	Operator.create({name: op[0], website: op[1]})
 end
 
-payment_methods = ["Bank Tarnsfer through WU", "Credit/Debit", "Bank Transfer"]
+payment_methods = ["Bank payment through WU", "Credit/debit card", "Bank transfer"]
 payment_methods.each do |method|
 	PaymentMethod.create({method: method})
 end
