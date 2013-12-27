1ce68f52ffb2bfb2316d52793a1633e84e9b2592
unknown <I840192@VANN00517798A.sap.corp>
2013-12-27 10:43:23 -0800

    xoom deployment ready

--- xoom/xoom_rates.py
+++ xoom/xoom_rates.py
@@ -4,5 +4,14 @@ import xoom_utils as Utils
 
 countries = [["IN", "INR"], ["PH", "PHP"], ["MX", "MXN"]]
 
+# CSV file to write scraped rates
+csv = open("../mt_rails_app/lib/assets/rates/xoom_rates.csv", "w")
+
+# For each country in the list of countries
 for country in countries:
+	# Extract rate and wite to csv file
     rate = Utils.get_rates(country[0], country[1])
+    csv.write("{0},{1}\n".format(country[0], rate))
+    print "Country: {0}, Rate: {1}".format(country[0], rate)
+
+csv.close()	# Close csv file
